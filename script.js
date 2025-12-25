64558b00c15f8a917c21d68dda73321dd220fd43
ElBangkitss <abangkiki1106@gmail.com>
2025-12-25 11:06:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const envelope = document.getElementById("envelope");
 const music = document.getElementById("music");
 
-music.volume = 0.25;
+music.volume = 0.25; // volume 25%
 
 envelope.addEventListener("click", () => {
   envelope.classList.add("open");
+
+  // trigger audio
+  if (music.paused) {
+    music.play().catch(() => {
+      console.log("Audio blocked, klik lagi untuk play");
+    });
+  }
 });
